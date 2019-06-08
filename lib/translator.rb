d00a8d5b927cadc5921335352625644ef9af7917
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-08 22:35:35 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,43 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+  library = YAML.load_file(path)
+
+  libHash = {"get_emoticon" => {}, "get_meaning" => {}}
+  library.each do |meaning, emojiArr|
+    libHash["get_meaning"][emojiArr[1]] = meaning
+    libHash["get_emoticon"][emojiArr[0]] = emojiArr[1]
+  end
+  libHash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, westEmoji)
+  dictionary = load_library(path)
+  #default the output to say the match was not found.
+  match = "Sorry, that emoticon was not found"
+
+  dictionary["get_emoticon"].each do |west, japan|
+    #only set the match output if the emoji is found
+    if west == westEmoji
+      match = japan
+    end
+  end
+  match
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, japanese_emoticon)
+  dictionary = load_library(path)
+
+  #default the output to say the match was not found.
+  match = "Sorry, that emoticon was not found"
+
+  dictionary["get_meaning"].each do |japan, meaning|
+    #only set the match output if the emoji is found
+    if japan == japanese_emoticon
+      match = meaning
+    end
+  end
+  match
 end
