fa69de4c0893fec00260de1194b58651210eaa32
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-08 22:17:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,19 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+  library = YAML.load_file(path)
+
+  libHash = {:get_meaning => {}, :get_emoticon => {}}
+  library.each do |meaning, emojiArr|
+    libHash[:get_meaning][emojiArr[1]] = meaning
+    libHash[:get_emoticon][emojiArr[0]] = emojiArr[1]
+  end
+  libHash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(load_library, western)
   # code goes here
 end
 
